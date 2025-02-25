ad8e4ac5fbe15562a9749c3fdbd11561bcb34293
Mateusz R <twojemail@przyklad.com>
2025-02-25 16:30:09 +0100

    dodanie klasy

---
+++ WiazanieKolekcji/Produkt.cs
@@ -0,0 +1,18 @@
+﻿namespace WiazanieKolekcji
+{
+    public class Produkt
+    {
+        public string Symbol { get; set; }
+        public string Nazwa { get; set; }
+        public int LiczbaSztuk { get; set; }
+        public string Magazyn { get; set; }
+
+        public Produkt(string symbol, string nazwa, int liczbaSztuk, string magazyn)
+        {
+            Symbol = symbol;
+            Nazwa = nazwa;
+            LiczbaSztuk = liczbaSztuk;
+            Magazyn = magazyn;
+        }
+    }
+}
